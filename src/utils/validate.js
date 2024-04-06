2b6f0dfa6b08f823e262856c8f7f54fa4c1380bf
pintu <pintu554123@gmail.com>
2024-04-06 12:49:53 +0530

    email password validation

---
+++ src/utils/validate.js
@@ -0,0 +1,9 @@
+
+export const validateData = (email, password) => {
+    const isValidEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const isValidPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+[\]{}|;:'",.<>?])(?=.*[0-9]).{8,}$/.test(password);
+
+    if (!isValidEmail) return "Email is not valid";
+    if (!isValidPassword) return "Password is not valid";
+    return null;
+}
